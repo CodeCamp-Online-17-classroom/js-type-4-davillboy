4620344213f4e80d6b0947662686d007363e3a3d
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 00:05:21 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // write code here
+        let num1 = prompt("กรุณากรอกตัวเลขที่ 1");
+        let num2 = prompt("กรุณากรอกตัวเลขที่ 2");
+
+        num1 = Number(num1);
+        num2 = Number(num2);
+
+        let sum = num1 + num2;
+
+        alert("ผลรวมตัวเลข: " + sum);
